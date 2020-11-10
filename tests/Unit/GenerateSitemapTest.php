ceca311c75bdb4152b2ebcb35cdd33deeb986ae0
Nolan Nordlund <nolan@nsquaredmedia.com>
2020-11-10 14:29:53 -0600

    Properly testing for the file

--- tests/Unit/GenerateSitemapTest.php
+++ tests/Unit/GenerateSitemapTest.php
@@ -3,7 +3,7 @@
 namespace ProjectRebel\AutomatedSitemap\Tests\Unit;
 
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 use ProjectRebel\AutomatedSitemap\Tests\TestCase;
 
 class GenerateSitemapTest extends TestCase
@@ -11,13 +11,16 @@ class GenerateSitemapTest extends TestCase
     /** @test */
     function testItCreatesASitemap()
     {
-        $this->assertFalse(Storage::exists('sitemap.xml'));
+        if (File::exists(public_path('sitemap.xml'))) {
+            File::delete(public_path('sitemap.xml'));
+        }
 
-        // Artisan::call('sitemap:generate');
+        $this->assertFalse(File::exists(public_path('sitemap.xml')));
+
         $this->artisan('sitemap:generate')
             ->expectsOutput('Generating sitemap...')
             ->assertExitCode(0);
 
-        // $this->assertTrue(Storage::exists('sitemap.xml'));
+        $this->assertTrue(File::exists(public_path('sitemap.xml')));
     }
 }
